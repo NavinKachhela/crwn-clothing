c26507976ab88cb8716bebc348e9fc710410a4e4
Navin H Kachhela <kachhelanavin@gmail.com>
2020-09-29 02:08:47 +0530

    Added Cart Item Function

--- src/App.js
+++ src/App.js
@@ -1,63 +1,66 @@
-import React from 'react';
-import './App.css';
-import { Switch , Route} from 'react-router-dom';
-import Header from './components/header/header.component';
-import HomePage from './pages/homepage/homepage.component.jsx';
-import ShopPage from './pages/shop/shop.component.jsx';
-import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import {auth,createUserProfileDocument} from './firebase/firebase.utils';
-class App extends React.Component {
-  constructor(){
-    super();
+import React from "react";
+import { Switch, Route, Redirect } from "react-router-dom";
+import { connect } from "react-redux";
+import "./App.css";
 
-    this.state = {
-      currentUser: null
-    };
-  }
+import HomePage from "./pages/homepage/homepage.component.jsx";
+import ShopPage from "./pages/shop/shop.component.jsx";
+import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
+import Header from "./components/header/header.component.jsx";
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils.js";
+import { setCurrentUser } from "./redux/user/user.actions";
+
+class App extends React.Component {
   unsubscribeFromAuth = null;
 
+  componentDidMount() {
+    const { setCurrentUser } = this.props;
 
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
 
-
-  componentDidMount(){
-    this.unsubscribeFromAuth = auth.onAuthStateChanged( async userAuth => {
-      if(userAuth){
-      const userRef = await createUserProfileDocument(userAuth);
-      userRef.onSnapshot(onSnapshot => {
-        this.setState({
-          currentUser: {
-            id: onSnapshot.id,
-            ...onSnapshot.data()
-          }
+        userRef.onSnapshot((snapShot) => {
+          setCurrentUser({
+            id: snapShot.id,
+            ...snapShot.data(),
+          });
         });
-      });
-      }
-      else{
-      this.setState({currentUser: userAuth});
+      } else {
+        setCurrentUser(userAuth);
       }
     });
   }
 
-
-
-
-  componentWillUnmount(){
+  componentWillUnMount = () => {
     this.unsubscribeFromAuth();
+  };
+  render() {
+    return (
+      <div>
+        <Header />
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route path='/shop' component={ShopPage} />
+          <Route exact path='/signin' 
+          render={()=>
+            this.props.currentUser ? (
+            <Redirect to='/' />
+          ) : (
+            <SignInAndSignUpPage />
+          )
+          }
+          />
+        </Switch>
+      </div>
+    );
   }
-  render(){
-    return(
-    <div>
-    <Header currentUser={this.state.currentUser}/>
-      <Switch>
-      <Route exact path='/' component={HomePage}/>
-      <Route path='/shop' component={ShopPage}/>
-      <Route path='/signin' component={SignInAndSignUpPage}/>
-      </Switch>
-    </div>
-  );
-  }
-    
 }
+const mapStateToProps = ({user})=> ({
+  current: user.CurrentUser
+})
+const mapDispatchToProps = (dispatch) => ({
+  setCurrentUser: (user) => dispatch(setCurrentUser(user)),
+});
 
-
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,27 +1,44 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
+import { auth } from "../../firebase/firebase.utils";
+import { connect } from 'react-redux';
 
-import { ReactComponent as Logo} from '../../assests/crown.svg';
-import {auth} from '../../firebase/firebase.utils';
-import './header.styles.scss';
+import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../card-dropdown/card-dropdown.component';
 
-const Header = ({ currentUser }) => (
-    <div className="header">
-        <Link className="logo-container" to="/">
-            <Logo className="logo"/>
-        </Link>
-        <div className="options">
-            <Link className="option" to="/shop">SHOP</Link>
-            <Link className="option" to="/shop">CONTACT</Link>
-            {
-                currentUser ? (
-                <div className="option" onClick={() => auth.signOut()}>SIGN OUT</div>
-                )
-                : (
-                <Link className="option" to='/signin'>SIGN IN</Link>
-                )
-            }
+import { ReactComponent as Logo } from "../../assests/crown.svg";
+import "./header.styles.scss";
+
+const Header = ({ currentUser,hidden }) => (
+  <div className='header'>
+    <Link className='logo-container' to='/'>
+      <Logo className='logo' />
+    </Link>
+    <div className='options'>
+      <Link className='option' to='/shop'>
+        SHOP
+      </Link>
+      <Link className='option' to='/shop'>
+        CONTACT
+      </Link>
+      {currentUser ? (
+        <div className='option' onClick={() => auth.signOut()}>
+          SIGN OUT
         </div>
+      ) : (
+        <Link className='option' to='/signin'>
+          SIGN IN
+        </Link>
+      )}
+      <CartIcon />
     </div>
+    { hidden ? null : <CartDropdown /> }
+  </div>
+
 );
-export default Header;
+
+const mapStateToProps = ({ user : {currentUser} , cart: {hidden} }) => ({
+  currentUser,
+  hidden
+})
+export default connect(mapStateToProps)(Header);
